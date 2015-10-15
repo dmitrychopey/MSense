a0bf30ccae493a74ba14475d83fedfb8ac84281c
Dmitry Chopey <dmitry.chopey@gmail.com>
2015-10-15 04:11:21 -0700

    Add logo and color

--- js/ML/GradientDescent.js
+++ js/ML/GradientDescent.js
@@ -1,1 +1,32 @@
-console.log(numeric.add([7,8,9],[10,1,2]));
+var data;
+
+function handleFileSelect(evt) {
+	
+   var file = evt.target.files[0];
+   Papa.parse(file, {
+      header: true,
+      dynamicTyping: true,
+      complete: function(results) {
+        data = results;
+      }
+    });
+
+  }
+
+
+  function calculate(){
+
+  	console.log(data);
+
+  }
+
+ 
+  $(document).ready(function(){
+    $("#csv-file").change(handleFileSelect);
+
+    $("#btn-calculate").click(calculate);
+  });
+
+
+
+   
